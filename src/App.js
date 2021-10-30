483f2f98245dc4d0addee511055988904e1375e8
Samuel Lau <samuel.cflau@gmail.com>
2021-10-30 23:34:38 +0800

    3-4 React 表單的基本應用

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [ inputValue, setInputValue ] = useState(0)
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value, console.log(e.target.value))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='card-header'>Network Speed Converter</div>
+      <div className='card-body'>
+
+        {/* unit-control */}
+        <div className='unit-control'>
+          <div className='unit'>Mbps</div>
+          <span className='exchange-icon fa-fw fa-stack'>
+            <i className='far fa-circle fa-stack-2x'></i>
+            <i className='fas fa-exchange-alt fa-stack-1x'></i>
+          </span>
+          <div className='unit'>MB/s</div>
+        </div>
+
+        {/* converter */}
+        <div className='converter'>
+          <div className='flex-1'>
+            <div className='converter-title'>Set</div>
+            <input
+              type='number'
+              onChange={handleInputChange}
+              value={inputValue}
+              className='input-number'
+              min='0'
+            />
+          </div>
+          <span className='angle-icon fa-2x' style={{ marginTop: '30px' }}>
+            <i className='fas fa-angle-right'></i>
+          </span>
+          <div className='text-right flex-1'>
+            <div className='converter-title'>Show</div>
+            <input
+              type='text'
+              className='input-number text-right'
+              disabled
+              value={inputValue / 8}
+            />
+          </div>
+        </div>
+      </div>
+      <div className='card-footer'>FAST</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
