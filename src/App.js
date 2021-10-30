5a7daa4cfa8e0fc3aa40fd5fe412b6556d52ddfe
Samuel Lau <samuel.cflau@gmail.com>
2021-10-31 00:51:43 +0800

    3-7 子層元件如何修改父層元件的資料狀態

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import UnitControl from './components/UnitControl'
+import UnitConverter from './components/UnitConverter'
 import CardFooter from './components/CardFooter'
 import './App.css'
 
@@ -15,34 +16,12 @@ const App = () => {
       <div className='card-header'>Network Speed Converter</div>
       <div className='card-body'>
         <UnitControl />
-
-        {/* converter */}
-        <div className='converter'>
-          <div className='flex-1'>
-            <div className='converter-title'>Set</div>
-            <input
-              type='number'
-              onChange={handleInputChange}
-              value={inputValue}
-              className='input-number'
-              min='0'
-            />
-          </div>
-          <span className='angle-icon fa-2x' style={{ marginTop: '30px' }}>
-            <i className='fas fa-angle-right'></i>
-          </span>
-          <div className='text-right flex-1'>
-            <div className='converter-title'>Show</div>
-            <input
-              type='text'
-              className='input-number text-right'
-              disabled
-              value={inputValue / 8}
-            />
-          </div>
-        </div>
+        <UnitConverter
+          inputValue={inputValue}
+          handleInputChange={handleInputChange}
+        />
       </div>
-      <CardFooter inputValue={inputValue}/>
+      <CardFooter inputValue={inputValue} />
     </div>
   )
 }
